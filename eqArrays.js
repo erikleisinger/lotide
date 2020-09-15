50ca68bc29fdfb430094651e8c752b6dbf24271c
Erik Leisinger <68292205+erikleisinger@users.noreply.github.com>
2020-09-15 15:37:25 -0400

    Update eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -11,10 +11,6 @@ const assertEqual = function(actual, expected) {
 const eqArrays = function(array1, array2) {
   let array1String = array1.join(' ');
   let array2String = array2.join(' ');
-  return assertEqual(array1String, array2String);
+  return array1String === array2String;
 };
-
-console.log(eqArrays([3, 4, 5],[4, 5, 6]));
-console.log(eqArrays([3, 4, 5],[3, 4, 5]));
-console.log(eqArrays([3, 4, 5],[5, 4, 3]));
 

f9569795b64198a09712e5621d524b32d4b90adf
erikleisinger <erikleisinger@gmail.com>
2020-09-18 22:09:47 +0000

    eqArrays with recursive function which compares arrays of arrays (subarrays?)

--- eqArrays.js
+++ eqArrays.js
@@ -9,8 +9,21 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
-  let array1String = array1.join(' ');
-  let array2String = array2.join(' ');
-  return array1String === array2String;
+  array1.forEach(c => {
+    if (Array.isArray(c)) {
+      array2.forEach(d => {
+        if (Array.isArray(d)) {
+          return eqArrays(c, d);
+        }
+      });
+    }
+  })
+      let array1String = array1.join(' ');
+      let array2String = array2.join(' ');
+      return array1String === array2String;
+    
 };
+
+console.log(eqArrays([5, 4, 3, [5, 5]], [5, 4, 3, [5, 5]]));
+console.log(eqArrays([5, 4, 3, [5, 5, 2]], [5, 4, 3, [5, 5]]));
 

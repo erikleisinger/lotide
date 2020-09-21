88adaba692fabaad37f0ad79ca1f3463ba9dbb39
erikleisinger <erikleisinger@gmail.com>
2020-09-21 16:24:24 +0000

    refactored middle.js so tests are in a separate file and folder.

--- middle.js
+++ middle.js
@@ -1,21 +1,3 @@
-
-const eqArrays = function(array1, array2) {
-  let array1String = array1.join(' ');
-  let array2String = array2.join(' ');
-  return array1String === array2String;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    return `✅️✅️✅️ Assertion Passed: "${array1}" === "${array2}"`;
-  } else {
-    return `❌️❌️❌️ Assertion Failed: "${array1}" !== "${array2}"`;
-  }
-
-};
-
-
-
 const middle = function (array) {
   let arrayOfMiddleElements = [];
   if (array.length % 2 === 0) {
@@ -29,5 +11,7 @@ const middle = function (array) {
     let oddMiddleIndex = Math.floor(array.length / 2);
     arrayOfMiddleElements.push(array[oddMiddleIndex]);
   }
-return arrayOfMiddleElements;
+  return arrayOfMiddleElements;
 };
+
+module.exports = middle;

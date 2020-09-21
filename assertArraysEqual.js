702f965f5b39d0069777fc45309fba4b116f911d
erikleisinger <erikleisinger@gmail.com>
2020-09-21 13:44:47 +0000

    assertArraysEqual refactored so that it imports the eqArrays function DRY

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,12 @@
-
-const eqArrays = function(array1, array2) {
-  let array1String = array1.join(' ');
-  let array2String = array2.join(' ');
-  return array1String === array2String;
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
-    return `✅️✅️✅️ Assertion Passed: "${array1}" === "${array2}"`;
+    console.log(`✅️✅️✅️ Assertion Passed: "${array1}" === "${array2}"`);
   } else {
-    return `❌️❌️❌️ Assertion Failed: "${array1}" !== "${array2}"`;
+    console.log(`❌️❌️❌️ Assertion Failed: "${array1}" !== "${array2}"`);
   }
 
 };
 
-console.log(assertArraysEqual([3, 4, 5],[4, 5, 6]));
-console.log(assertArraysEqual([3, 4, 5],[3, 4, 5]));
-console.log(assertArraysEqual([3, 4, 5],[5, 4, 3]));
+module.exports = assertArraysEqual;

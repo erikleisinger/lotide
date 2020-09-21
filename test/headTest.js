f3c8f56b43a757e3fad175e725ad8ea012cdc37f
erik <erikleisinger@gmail.com>
2020-09-21 13:04:40 -0400

    tests for head.js refactored to use mocha and chai.

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,17 @@
-const assertEqual = require("../assertEqual.js");
-const head = require("../head.js");
+const head = require("../head");
+const assert = require('chai').assert;
 
-
-assertEqual(head([1,1,1]), 1);
-assertEqual(head([1]), 1);
-assertEqual(head([]), 1);
+describe('head', () => {
+  it('should return 1 for the array [1,2,3]', () => {
+    assert.strictEqual(head([1,2,3]), 1);
+  });
+  it('returns 5 for the array [5]', () => {
+    assert.strictEqual(head([5]), 5);
+  });
+  it('FAIL TEST: the program will not output 4 when given array [5]', () => {
+    assert.strictEqual(head([5]), 4);
+  });
+  it('will output 1 for the array [1,1,1]', () => {
+    assert.strictEqual(head([1,1,1]), 1);
+  });
+});

c8c472d309e08943ab5362a7431ee053b873f7e4
erikleisinger <erikleisinger@gmail.com>
2020-09-21 16:59:40 +0000

    tests for tail.js and middle.js refactored to use mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,14 @@
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [ 3 ]);
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe('middle', () => {
+  it('should return 3 when passed the array [1,2,3,4,5] (returns the middle number)', () => {
+    assert.deepStrictEqual(middle([1,2,3,4,5]), [3]);
+  });
+  it('should return a blank array [] when passed a blank array []', () => {
+    assert.deepStrictEqual(middle([]), []);
+  });
+  it('should return an array of the middle two values [3,4] when passed an array with an even number of characters [1,2,3,4,5,6]', () => {
+    assert.deepStrictEqual(middle([1,2,3,4,5,6]), [3,4]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,11 @@
-const assertEqual = require("../assertEqual");
 const tail = require("../tail");
+const assert = require('chai').assert;
 
-assertEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
-assertEqual(tail([[1]]), []);
+describe('tail', () => {
+  it('should return the array [2,3,4,5] when passed [1,2,3,4,5]', () => {
+    assert.deepStrictEqual(tail([1,2,3,4,5]), [2,3,4,5]);
+  });
+  it('should return a blank array [] when passed an array with a single value [1]', () => {
+    assert.deepStrictEqual(tail([1]), []);
+  });
+});
